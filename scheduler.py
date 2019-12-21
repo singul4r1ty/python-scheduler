1b6904b7442ff23223f00dab617e37b05f8c2de0
Zak Karimjee <zak.karimjee@gmail.com>
2019-12-21 16:24:38 +0000

    Improved logging, added support for modulo-based timing

--- scheduler.py
+++ scheduler.py
@@ -1,34 +1,59 @@
-import csv, logging, datetime, time, subprocess, atexit
+# Script to run commands at regular intervals
+
+import csv
+import logging
+import datetime
+import time
+import subprocess
+import re
+import sys
 
 # open a logfile
-logfile = open("scheduler.log","a+")
-logfile.write("Starting scheduler at %s\n"%datetime.datetime.now())
+logging.basicConfig(level=logging.DEBUG,filename='scheduler.log',format='%(asctime)s - %(levelname)s: %(message)s')
+logging.info("Starting Scheduler")
+
+# setup for command line options
+verbose = False
 
 def main():
-    jobs = []
+    # read in and parse command line arguments using getopt
+    global verbose
+    cmd_args = sys.argv[1:]
+    if '-v' in cmd_args:
+        print("Verbose mode enabled: printing all logging to console")
+        verbose = True
     # read jobs from 'schedule'
+    jobs = []
     with open('schedule') as schedfile:
         sched_reader = csv.reader(schedfile,delimiter=" ",quotechar='"') # open schedule file
         jobs = [parse_job(row) for row in sched_reader] # parse each line and save in 'jobs' list
+    if verbose:
+        for job in jobs:
+            print("Job added: ", job["cmd"])
+
+
     # check once a minute if these jobs should be run
     starttime = time.time()
     while True:
-        print("Running jobs",datetime.datetime.now())
+        if verbose:
+            print("Running jobs",datetime.datetime.now())
         check_jobs(jobs)
         time.sleep(60.0 - ((time.time() - starttime)%60.0)) # sleep for the remaining 60 seconds
 
 def check_jobs(jobs):
     # function to loop over all the parsed jobs and run them if their specified timing matches the current time
+
+    # check month, date, hour and minute of job against the current time
     now = datetime.datetime.now()
     for job in jobs:
         runjob = False
-        if job["mo"] == "*" or int(job["mo"]) == now.month:
+        if match_parameter(job["mo"],now.month):
             runjob = True
-            if job["dy"] == "*" or int(job["dy"]) == now.day:
+            if match_parameter(job["dy"],now.day):
                 runjob = True
-                if job["hr"] == "*" or int(job["hr"]) == now.hour:
+                if match_parameter(job["hr"],now.hour):
                     runjob = True
-                    if job["mn"] == "*" or int(job["mn"]) == now.minute:
+                    if match_parameter(job["mn"],now.minute):
                         runjob = True
                     else:
                         runjob = False
@@ -40,10 +65,27 @@ def check_jobs(jobs):
             runjob = False
         if runjob:
             run_job(job) 
-    logfile.flush() 
+
+def match_parameter(input_param,match_param):
+    # this matches an input to the given parameter.
+    # if input_param is an integer, check if equal to match_param. If it is *, return true. If it is */<int>, check if it divides wholly into match_param
+    # this is used to either check if the timing given is an exact number, a wildcard or a multiple wildcard
+    if input_param=="*":
+        return True
+    elif re.match("\*\/.",input_param):
+        int_param = int(input_param.replace('/','').replace('*',''))
+        if match_param%int_param == 0:
+            return True
+        else:
+            return False
+    elif int(input_param)==match_param:
+        return True
+    else:
+        return False
 
 def parse_job(jobstring) -> dict:   
     # parse string from file into a dictionary, sanitising inputs
+
     job = {}
     assert(len(jobstring)>=5)
     # The job is defined by one line as follows: " <minute> <hour> <day> <month> <command to execute> "
@@ -54,7 +96,11 @@ def parse_job(jobstring) -> dict:
         try:
             int(jobstring[i])
         except ValueError:
-            if jobstring[i] != "*":
+            if jobstring[i] ==  "*":
+                pass
+            elif re.match("\*\/.",jobstring[i]):
+                pass
+            else:
                 raise ValueError("Job is invalid")
         # assign to relevant dictionary entry
         job[dn] = jobstring[i]
@@ -63,13 +109,12 @@ def parse_job(jobstring) -> dict:
     return(job)
 
 def run_job(job):
+    # simple helper function to run a specified job and log this to the file
+    global verbose
     subprocess.run(job["cmd"])
-    logfile.write("%s %s\n"%(datetime.datetime.now(),job["cmd"]))
-
-# close logfile when process is terminated
-def exit_handler():
-    logfile.close()
-atexit.register(exit_handler)
+    logging.info(" ".join(job["cmd"]))
+    if verbose:
+        print("Running job: ",job["cmd"])
 
 if __name__ == '__main__':
     main()
